16e15dd6d475650d7b95dc3c95f285f5dbb28c1a
Casto <castoraron255@gmail.com>
2025-08-09 11:29:05 +0300

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_date', 'published_date')
+    list_filter = ('author', 'published_date', 'created_date')
+    search_fields = ('title', 'text')
